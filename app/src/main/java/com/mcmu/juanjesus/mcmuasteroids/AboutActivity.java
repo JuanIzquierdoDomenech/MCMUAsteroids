76292445dc04462c4e690c34fa099fa2dab694c3
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2015-08-10 16:57:18 +0200

    Add overflow menu.

--- app/src/main/java/com/mcmu/juanjesus/mcmuasteroids/AboutActivity.java
+++ app/src/main/java/com/mcmu/juanjesus/mcmuasteroids/AboutActivity.java
@@ -1,19 +1,35 @@
 package com.mcmu.juanjesus.mcmuasteroids;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class AboutActivity extends AppCompatActivity {
 
+
+    //region Create
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
         // Intent extras test
-        Bundle extras = getIntent().getExtras();
-        String extrasString = extras.getString("SomeData");
-        Log.d("Intent extras: ", extrasString);
+        getTestIntent();
     }
+    //endregion
+
+
+    //region Intents
+    private void getTestIntent()
+    {
+        Bundle extras = getIntent().getExtras();
+        String extrasString = extras.getString("SendIntent");
+        Log.d("MCMUAsteroids", "Intent data (" + extrasString + ")");
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("ResponseIntent", "Intent response ok");
+        setResult(RESULT_OK, resultIntent);
+    }
+    //endregion
 }
--- app/src/main/java/com/mcmu/juanjesus/mcmuasteroids/MainActivity.java
+++ app/src/main/java/com/mcmu/juanjesus/mcmuasteroids/MainActivity.java
@@ -3,8 +3,11 @@ package com.mcmu.juanjesus.mcmuasteroids;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private Button btnExit;
     //endregion
 
+
+    //region Create
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +25,55 @@ public class MainActivity extends AppCompatActivity {
 
         linkUI();
     }
+    //endregion
 
+
+    //region Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //boolean response = false;
+        switch(id) {
+            case R.id.menu_settings:
+                break;
+            case R.id.menu_about:
+                showAboutActivity();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    //endregion
+
+
+    //region Intents
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data){
+        if (requestCode == 9999
+                && resultCode == RESULT_OK) {
+            String responseData = data.getExtras().getString("ResponseIntent");
+            Log.d("MCMUAsteroids", "Intent response returned successfully -> " + responseData);
+        }
+    }
+    //endregion
+
+
+    //region UI
     private void linkUI()
     {
         btnLaunchAboutActivity = (Button)findViewById(R.id.btn_asteroids_about);
         btnLaunchAboutActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent aboutActivityIntent = new Intent(v.getContext(), AboutActivity.class);
-                aboutActivityIntent.putExtra("SomeData", "This is some data :D");
-                startActivity(aboutActivityIntent);
+                showAboutActivity();
             }
         });
 
@@ -41,4 +85,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void showAboutActivity() {
+        Intent aboutActivityIntent = new Intent(getApplicationContext(), AboutActivity.class);
+        aboutActivityIntent.putExtra("SendIntent", "This is some data :D");
+        //startActivity(aboutActivityIntent);
+        startActivityForResult(aboutActivityIntent, 9999);
+    }
+    //endregion
+
 }
