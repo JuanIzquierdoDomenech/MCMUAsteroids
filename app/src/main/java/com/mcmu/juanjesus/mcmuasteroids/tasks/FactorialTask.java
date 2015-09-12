dd82f5d0d37b41dad257e4b4d0e2e32c17869e9d
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2015-09-12 20:28:41 +0200

    Make factorial async task cancellable.

--- app/src/main/java/com/mcmu/juanjesus/mcmuasteroids/tasks/FactorialTask.java
+++ app/src/main/java/com/mcmu/juanjesus/mcmuasteroids/tasks/FactorialTask.java
@@ -3,6 +3,7 @@ package com.mcmu.juanjesus.mcmuasteroids.tasks;
 import android.app.Activity;
 import android.app.Application;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.widget.Toast;
@@ -29,18 +30,24 @@ public class FactorialTask extends AsyncTask<Integer, Integer, Integer>{
 
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressDialog.setMessage("Loading...");
-        progressDialog.setCancelable(false);
+        progressDialog.setCancelable(true);
         progressDialog.setMax(100);
         progressDialog.setProgress(0);
         progressDialog.show();
 
+        progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                FactorialTask.this.cancel(true);
+            }
+        });
     }
 
     @Override
     protected Integer doInBackground(Integer... n) {
 
         int res = 1;
-        for (int i = 1; i <= n[0]; i++) {
+        for (int i = 1; i <= n[0] && !isCancelled(); i++) {
             res *= i;
             SystemClock.sleep(1000);
             publishProgress(i*100 / n[0]);
@@ -59,5 +66,10 @@ public class FactorialTask extends AsyncTask<Integer, Integer, Integer>{
 
         progressDialog.dismiss();
         Toast.makeText(callingActivity, "Factorial: " + result, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCancelled() {
+        
     }
 }
