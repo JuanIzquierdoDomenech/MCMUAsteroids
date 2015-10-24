ce47eed45c28fe5e145b1e93db751acde907897d
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2015-10-24 18:08:20 +0200

    Fix missing Strict mode declaration in LocalXAMMPWebServiceScoreStorage

--- app/src/main/java/com/mcmu/juanjesus/mcmuasteroids/score_storage/LocalXAMMPWebServiceScoreStorage.java
+++ app/src/main/java/com/mcmu/juanjesus/mcmuasteroids/score_storage/LocalXAMMPWebServiceScoreStorage.java
@@ -1,5 +1,6 @@
 package com.mcmu.juanjesus.mcmuasteroids.score_storage;
 
+import android.os.StrictMode;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -13,14 +14,22 @@ public class LocalXAMMPWebServiceScoreStorage implements ScoreStorage {
 
     private String host = "192.168.1.20";
 
+    public LocalXAMMPWebServiceScoreStorage() {
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+    }
+
     @Override
     public void saveScore(int score, String player, long date) {
+        Log.d("Asteroids", "SAVING");
         try {
             URL url = new URL("http://" + host + "/asteroids_scores/add_score.php" +
             "?newscore=" + score +
             "&player=" + URLEncoder.encode(player, "UTF-8") +
             "&date=" + date);
+            Log.d("Asteroids", url.toString());
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
             if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String line = reader.readLine();
@@ -38,9 +47,11 @@ public class LocalXAMMPWebServiceScoreStorage implements ScoreStorage {
 
     @Override
     public Vector<String> scoreList(int amount) {
+        Log.d("Asteroids", "LISTING");
         Vector<String> result = new Vector<>();
         try {
-            URL url = new URL("http://" + host + "/asteroids_scores/score_list?max=" + amount);
+            URL url = new URL("http://" + host + "/asteroids_scores/score_list.php?max=" + amount);
+            Log.d("Asteroids", url.toString());
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 
             if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
@@ -53,7 +64,7 @@ public class LocalXAMMPWebServiceScoreStorage implements ScoreStorage {
                 reader.close();
                 connection.disconnect();
             } else {
-                Log.e("Asteroids", connection.getResponseMessage());
+                Log.e("Asteroids", "CONNECTION ERROR: " + connection.getResponseMessage());
                 connection.disconnect();
             }
         } catch (Exception e) {
